712b4cbd5fd8c84b1e0f1ed8709aadf960ad2664
mrmrs <hi@mrmrs.cc>
2015-08-23 21:33:37 -0700

    Add build step to process css with postcss

---
+++ build.js
@@ -0,0 +1,38 @@
+// dependencies
+
+var fs = require("fs")
+var autoprefixer = require("autoprefixer")
+var postcss = require("postcss")
+var atImport = require("postcss-import")
+var cssvariables = require('postcss-css-variables')
+var compressor = require('node-minify')
+var conditionals = require('postcss-conditionals')
+var customMedia = require("postcss-custom-media")
+
+// css to be processed
+var css = fs.readFileSync("src/tachyons-spacing.css", "utf8")
+
+// process css
+var output = postcss([autoprefixer])
+  .use(atImport())
+  .use(cssvariables())
+  .use(conditionals())
+  .use(customMedia())
+  .process(css, {
+    from: "src/tachyons-spacing.css",
+    to: "css/tachyons-spacing.css"
+  })
+  .css
+
+fs.writeFile("css/tachyons-spacing.css", output, 'utf-8')
+
+// Using YUI Compressor for CSS
+new compressor.minify({
+    type: 'sqwish',
+    fileIn: 'css/tachyons-spacing.css',
+    fileOut: 'css/tachyons-spacing.min.css',
+    callback: function(err, min){
+    //console.log('Sqwish');
+    //console.log(err);
+    }
+});
